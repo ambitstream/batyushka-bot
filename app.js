0cebfeb93eafc05d05b83bd99dfefe09b0938127
Eugene Kholyavkin <eugene.kholyavkin@gmail.com>
2020-06-25 19:44:17 +0700

    remove webhook

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const path = require('path');
 const token = process.env.TOKEN;
-const port = process.env.PORT;
-const url = process.env.APP_URL
-const options = {
-  webHook: { port }
-};
 
 // Functions
 const randomNumber = max => Math.random() * max;
@@ -15,14 +10,12 @@ const getPhrase = src => src[Math.floor(randomNumber(source.length - 1))] || '';
 
 let source;
 fs.readFile(path.join(__dirname, 'bible.txt'), {encoding: 'utf8'}, (err, data) => {
-  if (err) throw err;
+	if (err) throw err;
 	source = data.split('\n');
 });
 
 if (token) {
-	const bot = new TelegramBot(token, options);
-
-	bot.setWebHook(`${url}/bot${token}`);
+	const bot = new TelegramBot(token, { polling: true });
 
 	bot.on('message', msg => {
 		const { id } = msg.chat;
